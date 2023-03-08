e5587e8f8d428c98b75c920f5782d93b5a40f23f
jztang <12689662+jztang@users.noreply.github.com>
2023-03-08 12:27:54 -0500

    Add function to play a single round

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let randomNum = Math.floor(Math.random() * 3);
+  let randomNum = Math.floor(Math.random() * 3); // get a random number 0-2
   let computerChoice;
 
   if (randomNum === 0) {
@@ -11,4 +11,40 @@ function getComputerChoice() {
   }
 
   return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  // normalize capitalization
+  playerSelection =
+    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+  computerSelection =
+    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
+
+  let result;
+
+  if (playerSelection === computerSelection) {
+    result = `It's a tie! ${playerSelection} equals ${computerSelection}`;
+  } else if (playerSelection === "Rock") {
+    if (computerSelection === "Paper") {
+      result = `You lose! ${playerSelection} loses to ${computerSelection}.`;
+    } else {
+      result = `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      result = `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+      result = `You lose! ${playerSelection} loses to ${computerSelection}.`;
+    }
+  } else if (playerSelection === "Scissors") {
+    if (computerSelection === "Rock") {
+      result = `You lose! ${playerSelection} loses to ${computerSelection}.`;
+    } else {
+      result = `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+  } else {
+    console.error("Bad player input");
+  }
+
+  return result;
 }
