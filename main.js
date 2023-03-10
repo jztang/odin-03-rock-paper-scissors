d757d7fd04b786c3f827639d305a4ee8ff1acedb
jztang <12689662+jztang@users.noreply.github.com>
2023-03-09 21:27:53 -0500

    Add function to play a full game

--- main.js
+++ main.js
@@ -21,30 +21,70 @@ function playRound(playerSelection, computerSelection) {
     computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
 
   let result;
+  let winner;
 
   if (playerSelection === computerSelection) {
     result = `It's a tie! ${playerSelection} equals ${computerSelection}`;
+    winner = "Tie";
   } else if (playerSelection === "Rock") {
     if (computerSelection === "Paper") {
       result = "You lose! Rock loses to Paper.";
+      winner = "Computer";
     } else if (computerSelection === "Scissors") {
       result = "You win! Rock beats Scissors.";
+      winner = "Player";
     }
   } else if (playerSelection === "Paper") {
     if (computerSelection === "Rock") {
       result = "You win! Paper beats Rock.";
+      winner = "Player";
     } else if (computerSelection === "Scissors") {
       result = "You lose! Paper loses to Scissors.";
+      winner = "Computer";
     }
   } else if (playerSelection === "Scissors") {
     if (computerSelection === "Rock") {
       result = "You lose! Scissors loses to Rock.";
+      winner = "Computer";
     } else if (computerSelection === "Paper") {
       result = "You win! Scissors beats Paper.";
+      winner = "Player";
     }
   } else {
     console.error("Bad player input");
   }
 
-  return result;
+  return { result, winner };
 }
+
+function game() {
+  let playerPoints = 0;
+  let computerPoints = 0;
+
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt(`Round ${i}: Pick Rock/Paper/Scissors`, "Rock");
+    let { result, winner } = playRound(playerSelection, getComputerChoice());
+
+    if (winner === "Player") {
+      playerPoints++;
+    } else if (winner == "Computer") {
+      computerPoints++;
+    }
+
+    console.log(`[${playerPoints}-${computerPoints}] ${result}`);
+  }
+
+  let finalResult;
+
+  if (playerPoints > computerPoints) {
+    finalResult = "You win!";
+  } else if (playerPoints < computerPoints) {
+    finalResult = "You lose.";
+  } else {
+    finalResult = "It's a tie!";
+  }
+
+  console.log(`Final score: ${playerPoints}-${computerPoints}. ${finalResult}`);
+}
+
+game();
